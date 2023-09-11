ec2e0db32a5c71d4bc5be1a05d17f12b77c82def
serg-n3 <ktg80@yandex.ru>
2023-09-11 18:15:59 +0300

    2.2. Модель данных - Post. [#285211]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -19,11 +19,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title) && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, created);
+        return Objects.hash(id, link);
     }
 }
