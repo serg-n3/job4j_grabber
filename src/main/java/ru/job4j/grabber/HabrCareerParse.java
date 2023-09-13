3dca056536860e82eb6ae88ef9f8cb5d7b0bfc48
serg-n3 <ktg80@yandex.ru>
2023-09-13 16:47:47 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,13 +5,24 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
-public class HabrCareerParse {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     private static String retrieveDescription(String link) throws IOException {
         Connection connection = Jsoup.connect(link);
@@ -20,27 +31,42 @@ public class HabrCareerParse {
         return element.text();
     }
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 6; i++) {
-            String newLink = String.format(PAGE_LINK + "?page=%s", i);
-                Connection connection = Jsoup.connect(newLink);
-                Document document = connection.get();
-                Elements rows = document.select(".vacancy-card__inner");
-                rows.forEach(row -> {
-                    Element titleElement = row.select(".vacancy-card__title").first();
-                    Element linkElement = titleElement.child(0);
-                    String vacancyName = titleElement.text();
-                    String vacancyDate = row.select(".vacancy-card__date time").first().attr("datetime");
-                    String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                    String description;
-                    try {
-                         description = retrieveDescription(link);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.printf("%s %s %s%n%s%n", vacancyName, link, vacancyDate, description);
-                });
-            }
-
+    public List<Post> createPost() throws IOException {
+        List<Post> postList = new ArrayList<>();
+        for (int i = 1; i < 6; i++) {
+            postList.addAll(list(String.format(PAGE_LINK + "?page=%s", i)));
         }
+        return postList;
     }
+
+    public static void main(String[] args) throws IOException {
+        HabrCareerParse habrCareerParse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> list = habrCareerParse.createPost();
+        list.forEach(System.out::println);
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> list = new ArrayList<>();
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            String vacancyName = titleElement.text();
+            String vacancyDate = row.select(".vacancy-card__date time").first().attr("datetime");
+            String newLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String description;
+            try {
+                description = retrieveDescription(link);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Post post = new Post(vacancyName, newLink, description, dateTimeParser.parse(vacancyDate));
+            list.add(post);
+        });
+
+        return list;
+    }
+}
--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -5,10 +5,65 @@ import java.util.Objects;
 
 public class Post {
     private int id;
-   private String title;
-   private String link;
-   private String description;
-   private LocalDateTime created;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
 
     @Override
     public boolean equals(Object o) {
